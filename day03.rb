01709e589f7e6789aa3b24091da669bda5c18605
Philippe Creux <pcreux@gmail.com>
2025-12-04 21:59:41 +0100

    day03

--- day03.rb
+++ day03.rb
@@ -6,20 +6,28 @@ INPUT = <<~STR
 818181911112111
 STR
 
-def largest_number(digits)
-  digit_1 = digits[0..-2].sort.reverse.first
-  index_digit_1 = digits.index(digit_1)
-  digit_2 = digits[(index_digit_1 + 1)..-1].sort.reverse.first
+def largest_number(digits, max: , acc: 0)
+  # puts "largest_number(#{digits}, max: #{max}, acc: #{acc}"
+  digit = digits[0..-max].sort.last
+  index_digit = digits.index(digit)
 
-  digit_1 * 10 + digit_2
+  current = acc * 10 + digit
+  return current if max == 1
+
+  largest_number(digits[(index_digit+1)..-1], max: max - 1, acc: current)
 end
 
-def run(input)
+def run(input, max: 2)
   input.split("\n").map do |line|
     digits = line.split("").map(&:to_i)
-    largest_number(digits)
+    largest_number(digits, max:)
   end.sum
 end
 
+puts "With 2:"
 puts run(INPUT)
 puts run(File.read('day03.txt'))
+
+puts "With 12:"
+puts run(INPUT, max: 12)
+puts run(File.read('day03.txt'), max: 12)
