3161bec1153376893d07e7ffd9c2ab2a6c2c545b
Philippe Creux <pcreux@gmail.com>
2025-12-04 22:36:00 +0100

    day04 - part2

--- day04.rb
+++ day04.rb
@@ -37,6 +37,13 @@ class Grid
     row[x] if row
   end
 
+  def remove(x, y)
+    return nil if x < 0 || y < 0
+
+    row = grid[y]
+    row[x] = "x" if row
+  end
+
   def around(x, y)
     [-1, 0, 1, -1, 1].permutation(2).uniq.map do |shift_x, shift_y|
       next if shift_x == 0 && shift_y == 0
@@ -65,5 +72,40 @@ def run(input)
   puts count
 end
 
+def find_rolls(grid)
+  rolls = []
+
+  grid.max_x.times do |x|
+    grid.max_y.times do |y|
+      next unless grid.get(x, y) == "@"
+
+      count = grid.around(x, y).count("@")
+      rolls << [x, y] if count < 4
+    end
+  end
+
+  rolls
+end
+
+def run_2(input)
+  grid = Grid.build(input)
+  count = 0
+
+  loop do
+    rolls = find_rolls(grid)
+    break if rolls.empty?
+
+    rolls.each do |x, y|
+      grid.remove(x, y)
+      count += 1
+    end
+  end
+
+  puts count
+end
+
 run(INPUT)
 run(File.read('day04.txt'))
+
+run_2(INPUT)
+run_2(File.read('day04.txt'))
